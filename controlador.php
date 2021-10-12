6586d3c90420d87cf54e2aaa3d46a1f37f5cab75
Gustavo Torres <edythor@gmail.com>
2021-10-12 16:12:22 -0500

    Se refactorizó el controlador del robot y se añadio el comando /dharma usando la clase delfos

--- controlador.php
+++ controlador.php
@@ -2,11 +2,15 @@
 
 //Este archivo en la carpeta padre que es inaccesecible
 include('../../bot_config.php'); 
+include ('models/delfos.php');
+include ('libs/Misc.php');
+
 
 //Obtenemos el token almacenado en un lugar no accesible publicamente
 $token = $config['token'];
 //En caso de que haya problemas poner debug en true
 $debug=false;
+$salto_de_linea=PHP_EOL;
 
 //Link de la api de Telegram
 $website = 'https://api.telegram.org/bot'.$token;
@@ -27,7 +31,7 @@ if($debug)
     $parameters = array(
         "chat_id" => $chatId,
         "text" => $message,
-        "parseMode" => "html"
+        "parse_mode" => "html"
     );
 
     sendMessage("sendMessage", $parameters);
@@ -38,6 +42,7 @@ $update = json_decode($input, TRUE);
 
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
+$nombre=$update['message']['chat']['first_name'];
 
 switch($message) 
 {
@@ -47,29 +52,80 @@ switch($message)
         $parameters = array(
         "chat_id" => $chatId,
         "text" => $message,
-        "parseMode" => "html"
+        "parse_mode" => "html"
         );
         
         sendMessage("sendMessage", $parameters);
         
         break;
     case '/info':
-        $message = 'Hola! Soy El Oraculo de Delfos';
+        $message = 'Hola! Soy el Oráculo de Delfos';
         $parameters = array(
         "chat_id" => $chatId,
         "text" => $message,
-        "parseMode" => "html"
+        "parse_mode" => "html"
         );
         sendMessage("sendMessage", $parameters);
         break;
     default:
-        $message = 'No te he entendido';
+
+
+        if (strpos($message, "/dharma") === 0 ) 
+        {
+                $fecha_nacimiento = substr($message, 8);
+            
+                $arreglo_fecha = explode("/", $fecha_nacimiento);
+    
+                $resultado=Misc::validar_fecha($arreglo_fecha);
+    
+             if($resultado["respuesta"])
+             {
+                //En este momento la fecha pasó la validación pero
+                //  la respuesta nos trae el dia mes y año con digitos
+                // sin cero adelante o alfanumericos atras y en string
+                $dia=$resultado["dia"];
+                $mes=$resultado["mes"];
+                $ano=$resultado["ano"];
         
-        $parameters = array(
-        "chat_id" => $chatId,
-        "text" => $message,
-        "parseMode" => "html"
-        );
+                $mensaje.="";
+        
+                $delfos = new delfos($dia,$mes,$ano);
+    
+                $mensaje.= "<b>".$nombre."</b> ".$delfos->getPersonalidad().$salto_de_linea;
+                $mensaje.= $salto_de_linea."<b>En cuanto a tu alma:</b>".$salto_de_linea;
+                $mensaje.= $delfos->getAlma().$salto_de_linea;
+                $mensaje.= $salto_de_linea."<b>Estos son tus dones:</b>".$salto_de_linea;
+                $mensaje.= $delfos->getRegalo().$salto_de_linea;
+                $mensaje.= $salto_de_linea."<b>Tu viniste a este mundo a:</b>".$salto_de_linea;
+                $mensaje.= $delfos->getProposito();
+         
+                
+            }
+            else
+            {
+                $mensaje=$resultado["msg"];  
+            }
+            
+            
+            $parameters = array(
+            "chat_id" => $chatId,
+            "text" => $mensaje,
+            "parse_mode" => "HTML"
+            );    
+            
+        }
+        else
+        {
+          $message = '<i>No te he entendido</i>';
+        
+            $parameters = array(
+            "chat_id" => $chatId,
+            "text" => $message,
+            "parse_mode" => "html"
+            );    
+        }
+        
+        
         sendMessage("sendMessage", $parameters);
         break;
 }
